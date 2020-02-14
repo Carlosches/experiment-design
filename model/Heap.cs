703234f296e3f22dcd3e0153355b3918c3ff6e61
Cesar Canales <sleepata6@gmail.com>
2020-02-14 11:29:54 -0500

    Implemented merge and heap sort in c#

---
+++ model/Heap.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public class Heap<E>
+{
+	private E[] tree;
+	private int heapSize;
+	private Icomparer<E> comp;
+
+	public Heap(Icomparer<E> comp)
+	{
+		this.comp = comp;
+	}
+
+	public int left(int index) {
+		return 2*index + 1;
+	}
+	
+	public int right(int index) {
+		return 2*(index+1);
+	}
+	
+	public int parent(int index) {
+		if(index == 0) { //root does not have a parent
+			return Integer.MIN_VALUE;
+		}
+		return (index - 1)/2;
+	}
+	
+	//max seria min si se inicializo como un min-heap
+	public void heapify(int index) {
+		int l = left(index);
+		int r = right(index);
+		int max = index;
+		if(l < heapSize && comp.compare(tree[l], tree[max]) > 0) {
+			max = l;
+		}
+		if(r < heapSize && comp.compare(tree[r], tree[max]) > 0) {
+			max = r;
+		}
+		
+		E current = tree[index];
+		if(comp.compare(tree[max], current) != 0) {
+			tree[index] = tree[max];
+			tree[max] = current;
+			heapify(max);
+		}
+	}
+	
+	public E[] getTree() {
+		return tree;
+	}
+
+	public void buildHeap(E[] array) {
+		int firstInnerNode = parent(tree.length-1);
+		heapSize = tree.length;
+		while(firstInnerNode > -1) {
+			heapify(firstInnerNode);
+			firstInnerNode--;
+		}
+	}
+	
+	public static void HeapSort(T[] array, Comparator<T> comp) {
+		 Heap<T> h = new Heap<T>(comp);
+		 h.setTree(array); //llamada implicita a buildHeap
+		 T[] tarray = h.getTree();
+		 for (int i = tarray.length-1; i > 0; i--) { //desde
+			T root = tarray[0];
+			tarray[0] = tarray[h.heapSize-1];
+			tarray[h.heapSize-1] = root;
+			h.heapSize--;
+			h.heapify(0);
+		}
+	}
+	
+	public E extractRoot() {
+		if(heapSize < 1) {
+			throw new Exception("Underflow");
+		}
+		E root = tree[0];
+		tree[0] = tree[heapSize-1];
+		heapSize--;
+		heapify(0);
+		return root;
+	}
+	
+	public void increaseKey(int index, E key){
+		if(comp.compare(key, tree[index]) < 0) {
+			throw new Exception("key is less than current key");
+		}
+		tree[index] = key;
+		while(index > 0 && comp.compare(tree[parent(index)], tree[index]) < 0) {
+			E p = tree[parent(index)];
+			tree[parent(index)] = tree[index];
+			tree[index] = p;
+			index = parent(index);
+		}
+	}
+	
+	public void heapInsert(E key) {
+		heapSize++;
+		tree[heapSize-1] = key; //el menor de los menores o el mayor de los mayores, segun sea el caso
+		increaseKey(heapSize - 1, key);
+	}
+	
+	//TODO use buil-heap
+	public void setTree(E[] array) {
+		tree = array;
+		buildHeap(tree);
+	}
+
+	public int getHeapSize() {
+		return heapSize;
+	}
+
+	public void setHeapSize(int heapSize) {
+		this.heapSize = heapSize;
+	}
+
+	public Comparator<E> getComp() {
+		return comp;
+	}
+
+	public void setComp(Comparator<E> comp) {
+		this.comp = comp;
+	}	
+}
---
+++ model/MergeSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class MergeSort
+{
+	public MergeSort()
+	{
+	}
+
+    public void Merge(E[] array, int left, int middle, int right, Icomparer<E> comp)
+    {
+        E[] leftArray = new E[middle - left + 1];
+        E[] rightArray = new E[right - middle];
+
+        Array.Copy(array, left, leftArray, 0, middle - left + 1);
+        Array.Copy(array, middle + 1, rightArray, 0, right - middle);
+
+        int i = 0;
+        int j = 0;
+        for (int k = left; k < right + 1; k++)
+        {
+            if (i == leftArray.Length)
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+            else if (j == rightArray.Length)
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else if (comp.compare(leftArray[i], rightArray[j]) <=0)
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+        }
+    }
+
+    public void MergeSort(E[] array, int left, int right, Icomparer<E> comp)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+
+            MergeSort(array, left, middle, comp);
+            MergeSort(array, middle + 1, right, comp);
+
+            Merge(array, left, middle, right, comp);
+        }
+    }
+}
